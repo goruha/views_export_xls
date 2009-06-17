fa59a0397f3a3e2fb50a14447025afc547e81244
Johnny Kartakov <jk_arsum_com@350147.no-reply.drupal.org>
2009-06-17 08:10:08 +0000

    Initial commit of views export xls module.

---
+++ README.txt
@@ -0,0 +1,15 @@
+/* $Id$ */
+
+-- SUMMARY --
+
+Allow to export views data to excel
+
+
+-- REQUIREMENTS --
+
+None.
+
+
+-- INSTALLATION --
+
+* Install as usual, see http://drupal.org/node/70151 for further information.
---
+++ views-export-xls-file.tpl.php
@@ -0,0 +1,26 @@
+<?php
+// $Id$
+/**
+ * @file views-export-xls-file.tpl.php
+ * Template to display a view as an xls file.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $rows: An array of row items. Each row is an array of content
+ *   keyed by field ID.
+ * - $header: an array of haeaders(labels) for fields.
+ * - $themed_rows: a array of rows with themed fields.
+ * @ingroup views_templates
+ */
+  require_once('./' . drupal_get_path('module', 'views_export_xls') . '/ExcelExport.class.php');
+
+  $xls = new ExcelExport();
+  //add hearder
+  $xls->addRow(array_values($header));
+  //add rows
+  foreach ($themed_rows as $count => $row) {
+    $xls->addRow(array_values($row));
+  }
+  
+  $xls->output();
+
+?>
